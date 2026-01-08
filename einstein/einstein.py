bdb30bf989375c4c5626162e2b25f5ce03b0f6a4
vishvendrasingh00 <svishvendra2020@gmail.com>
2026-01-08 13:11:16 +0000

    Solved Einstein

---
+++ einstein/einstein.py
@@ -0,0 +1,11 @@
+# Ask the user for mass (convert it to an integer immediately)
+m = int(input("m: "))
+
+# Define the speed of light
+c = 300000000
+
+# Calculate Energy (E = m * c squared)
+e = m * (c ** 2)
+
+# Print the result
+print(e)
